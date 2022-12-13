ff3bafbf915f300147786cbd7e15f6a9d09478be
khoubaib <khoubaib15@outlook.com>
2022-12-13 21:38:14 +0000

    some more js added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,13 @@
 var currentDayEl = $("#currentDay");
     var currentDay = moment().format("dddd Do MMMM YYYY");
     currentDayEl.text(currentDay);
+
+var startOfBusiness = moment(09, 'hh');
+
+while (startOfBusiness.hour() < 18) {
+    console.log(startOfBusiness.format('hha'));
+
+    startOfBusiness.add(1, 'hours');
+
+    console.log(moment().format("HH").isBefore(startOfBusiness));
+}
